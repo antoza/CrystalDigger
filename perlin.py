ed909c803dfa403d0999229fa39197b0ec2c4040
Romain Schwarz <Romain.Schwarz@grenoble-inp.org>
2022-03-28 18:02:28 +0200

    Correction du bruit de Perlin 1D

--- perlin.py
+++ perlin.py
@@ -1,31 +1,21 @@
 #!/usr/bin/env python3
 import random
+from math import sqrt
 
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 
 
-# class UnitVect:
-#
-#     def __init__(self, dim=1):
-#         self.vect = vect = [random.random() for _ in range(dim)]
-#         self.normalize()
-#
-#     def normalize(self):
-#         normal = sum(self.vect)
-#         for elt in self.vect:
-#             elt /= normal
-#
-#     def unit_vect(self):
-#         return self.vect
-
-
 def unit_vect(dim):
-    vect = [random.random() for _ in range(dim)]
-    normal = sum(vect)
-    for elt in vect:
-        elt /= normal
+    vect = []
+    normal = 0
+    for _ in range(dim):
+        value = 2 * random.random() - 1
+        vect.append(value)
+        normal += value**2
+    for i in range(dim):
+        vect[i] /= sqrt(normal)
     return vect
 
 
@@ -33,47 +23,76 @@ def interpolate(x, y, c):
     return c * x + (1 - c) * y
 
 
+def dot_prod(vect1, vect2):
+    prod = 0
+    for i in range(len(vect1)):
+        prod += vect1[i] * vect2[i]
+    return prod
+
+
 class Perlin1d:
 
-    def __init__(self, octaves=4, n=16, seed=0):
+    def __init__(self, n=16, seed=0):
         # Initialize the random array with unit vectors
         self.size = n
         random.seed(seed)
         self.grad = np.array([unit_vect(1) for _ in range(n)])
-        self.values = np.zeros(n)
+        print(self.grad)
 
-        for o in range(min(octaves, int(np.log2(n)))):
-            i_min = 0
-            i_max = int(n / (2 ** o))
-            step = i_max
-            # Generate the whole octave
-            while i_max <= n:
-                for i in range(i_min, i_max):
-                    interpolation_coeff = (i - i_min) / (i_max - i_min)
-                    amp_factor = step/n
-                    self.values[i] += amp_factor * interpolate(self.grad[i_min][0],
-                                                               self.grad[i_max % n][0],
-                                                               interpolation_coeff)
-                i_min += step
-                i_max += step
+        # for o in range(min(octaves, int(np.log2(n)))):
+        #     print("Octave : ", o+1)
+        #     i_min = 0
+        #     i_max = int(n / (2 ** o))
+        #     step = i_max
+        #     # Generate the whole octave
+        #     while i_max <= n:
+        #         print(i_min, " ", i_max)
+        #         for i in range(i_min, i_max):
+        #             interpolation_coeff = (i - i_min) / (i_max - i_min)
+        #             amp_factor = step/n
+        #             self.values[i] += amp_factor * interpolate(self.grad[i_min][0],
+        #                                                        self.grad[i_max % n][0],
+        #                                                        interpolation_coeff)
+        #         i_min += step
+        #         i_max += step
 
     def noise(self, x):
-        # For each octave, add the corresponding value to the noise vector
-        index = int(int(x * self.size) % self.size)
-        c = x * self.size - int(x * self.size)
-        return interpolate(self.values[index], self.values[int((index + 1) % self.size)], c)
+        # x has to be in [0, 1[
+        bounded_x = x - int(x)
+
+        # Calcul de l'indice du noeud inférieur
+        index = int(bounded_x * (self.size - 1))
+
+        # Calcul de la distance entre le point et les noeuds voisins
+        previous_value = index / (self.size - 1)
+        next_value = (index + 1) / (self.size - 1)
+        dist_node1 = bounded_x - previous_value
+        dist_node2 = next_value - bounded_x
+
+        # Coefficient d'interpolation
+        c = dist_node2 / (next_value - previous_value)
+
+        return interpolate(self.grad[index] * dist_node1, self.grad[index + 1] * dist_node2, c)
 
 
 def main():
-    perlin = Perlin1d(octaves=2, n=2048)
-    result = []
-    image = []
-    for t in np.arange(0, 1.01, .01):
-        result.append(perlin.noise(t))
-    for t in np.arange(0, 1.01, .01):
-        image.append(result)
-    #plt.plot(np.array(image))
-    plt.imshow(np.array(image))
+    perlin = Perlin1d(n=4)
+
+    n_max = 2000
+    result = [0 for _ in range(n_max)]
+    f = 1
+    step = 1/n_max
+
+    for p in range(4):
+        freq_factor = 2**p
+        amp_factor = 1 / (2**p)
+        for i in range(n_max):
+            result[i] += amp_factor * perlin.noise(freq_factor * f * step * i)
+
+    for i in range(n_max):
+        result[i] += 1
+
+    plt.plot(result)
     plt.show()
 
 
