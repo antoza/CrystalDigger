920376e5f79a99942a41b361a715b83a2a706e9d
Romain Schwarz <Romain.Schwarz@grenoble-inp.org>
2022-04-04 22:51:42 +0200

    Premiers essais pour la constitution de la scène

---
+++ scene.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from core import Shader, Viewer, Mesh, load, Node
+from transform import rotate, translate, scale
+
+from viewer import Surface
+
+
+def generate_walls(shader, rotation=rotate((0, 0, 1), 0), n_x=10, n_y=10):
+    offset_x = n_x / 2
+    offset_x -= offset_x / n_x
+    offset_y = n_y / 2
+    offset_y -= offset_y / n_y
+
+    scale_x = 30
+    scale_y = 30
+
+    r = rotate((1, 0, 0), 45)
+    s = scale(1 / scale_x, 1 / scale_y, min(1 / scale_x, 1 / scale_y))
+    Walls = Node(transform = rotation)
+    for i in range(-1, n_x + 1):
+        t = translate(-offset_x + i, offset_y, offset_x / n_x)
+        Wall = Node(transform=t @ r @ s)
+        Wall.add(Surface(shader, n_x=scale_x, n_y=scale_y))
+        Walls.add(Wall)
+    return Walls
+
+
+def generate_scene(shader):
+    scene = Node(transform=rotate((1, 0, 0), -30) @ translate(0, 5, -10))
+
+    # adding the floor
+    n_x = 10
+    n_y = 10
+    scene.add(Surface(shader, amp=0, n_x=n_x, n_y=n_y))
+
+    upper_walls = generate_walls(shader, n_x=n_x, n_y=n_y)
+    right_walls = generate_walls(shader, n_x=n_x, n_y=n_y, rotation=rotate((0, 0, 1), -90))
+    left_walls = generate_walls(shader, n_x=n_x, n_y=n_y, rotation=rotate((0, 0, 1), 90))
+    lower_walls = generate_walls(shader, n_x=n_x, n_y=n_y, rotation=rotate((0, 0, 1), 180))
+    scene.add(upper_walls)
+    scene.add(right_walls)
+    scene.add(left_walls)
+    scene.add(lower_walls)
+
+    return scene
+
+
+def main():
+    viewer = Viewer()
+    shader = Shader("texture.vert", "texture.frag")
+
+    scene = generate_scene(shader)
+
+    viewer.add(scene)
+
+    viewer.run()
+
+
+if __name__ == "__main__":
+    main()
--- viewer.py
+++ viewer.py
@@ -7,14 +7,20 @@ import numpy as np  # all matrix manipulations & OpenGL args
 from core import Shader, Viewer, Mesh, load
 from texture import Texture, Textured
 from perlin import Perlin2d
+from transform import vec
+from math import sqrt
 
 
 # -------------- Notre petit jeu fait maison :) ---------------------------------
 
-class Floor(Textured):
+def norm(vect):
+    return sqrt(vect[0]**2 + vect[1] ** 2 + vect[2] ** 2)
 
-    def __init__(self, shader, tex=None):
-        perlin = Perlin2d()
+class Surface(Textured):
+
+    def __init__(self, shader, tex=None, amp=1, n_x=30, n_y=30):
+        n = 16
+        perlin = Perlin2d(n=n)
 
         # prepare texture modes cycling variables for interactive toggling
         self.wraps = cycle([GL.GL_REPEAT, GL.GL_MIRRORED_REPEAT,
@@ -25,23 +31,23 @@ class Floor(Textured):
         self.wrap, self.filter = next(self.wraps), next(self.filters)
         #self.tex = tex
 
-        f = .1
+        f = .2
 
         vert = []
         normals = []
         indices = []
-        amp = 1
-        n_x = 30  # number of squares x-axis
-        n_y = 30  # number of squares y-axis
         off_x = n_x / 2
         off_y = n_y / 2
         for j in range(n_y):
             for i in range(n_x):
                 # heights of the (possibly) duplicated vertices
-                z1 = amp * perlin.noise(f * i, f * j) + amp * perlin.noise(2 * f * i, 2 * f * j)
-                z2 = amp * perlin.noise(f * (i + 1), f * j) + amp * perlin.noise(2 * f * (i+1), 2 * f * j)
-                z3 = amp * perlin.noise(f * i, f * (j+1)) + amp * perlin.noise(2 * f * i, 2 * f * (j+1))
-                z4 = amp * perlin.noise(f * (i+1), f * (j+1)) + amp * perlin.noise(2 * f * (i+1), 2 * f * (j+1))
+                I = i
+                J = j
+
+                z1 = amp * perlin.noise(f * I, f * J) + amp * perlin.noise(2 * f * I, 2 * f * J)
+                z2 = amp * perlin.noise(f * (I + 1), f * J) + amp * perlin.noise(2 * f * (I+1), 2 * f * J)
+                z3 = amp * perlin.noise(f * I, f * (J+1)) + amp * perlin.noise(2 * f * I, 2 * f * (J+1))
+                z4 = amp * perlin.noise(f * (I+1), f * (J+1)) + amp * perlin.noise(2 * f * (I+1), 2 * f * (J+1))
 
                 # vertices of the first triangle
                 vert.append((i - off_x, j - off_y, z1))
@@ -54,8 +60,20 @@ class Floor(Textured):
                 vert.append((i - off_x, j + 1 - off_y, z3))
 
                 # normals of the first triangle
-                normal1 = [1, 1, z2 + z3 - 2*z1]
-                normal2 = [1, 1, z4 + z2 - 2*z3]
+                zn1 = z2 + z3 - 2*z1
+                zn2 = z4 + z2 - 2*z3
+                if zn1 == 0:
+                    normal1 = vec((0, 0, 1))
+                else:
+                    normal1 = vec([1, 1, zn1])
+                    normal1 /= norm(normal1)
+
+                if zn2 == 0:
+                    normal2 = vec((0, 0, 1))
+                else:
+                    normal2 = vec([1, 1, zn2])
+                    normal2 /= norm(normal2)
+
                 normals.append(normal1)
                 normals.append(normal1)
                 normals.append(normal1)
@@ -71,7 +89,7 @@ class Floor(Textured):
         self.indices = np.array(indices)
 
         # Defining the material and the light_direction
-        uniforms = dict({"k_a": (.25, .21, .21), "k_d": (1, .83, .83), "k_s": (.3, .3, .3), "s": .088, "light_dir": (0, 0, -1)})
+        uniforms = dict({"k_a": (.25, .21, .21), "k_d": (1, .83, .83), "k_s": (.3, .3, .3), "s": .088, "light_dir": (-1, -.2, -1)})
 
         mesh = Mesh(shader, attributes=dict(position=self.vert, normal=self.normals),
                     index=self.indices, uniforms=uniforms)
@@ -99,7 +117,7 @@ def main():
 
     light_dir = (0, 0, -1)
     viewer.add(*[mesh for file in sys.argv[1:] for mesh in load(file, shader, light_dir=light_dir)])
-    viewer.add(Floor(shader))
+    viewer.add(Surface(shader))
 
     # start rendering loop
     viewer.run()
