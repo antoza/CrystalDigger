610cd23ede854c15b899f813e447ad8eb7a151a2
Antony Zahran <antony.zahran@grenoble-inp.org>
2022-04-09 15:02:43 +0000

    Replace Jeu_de_nain.py

--- Jeu_de_nain.py
+++ Jeu_de_nain.py
@@ -1,3 +1,5 @@
+from entity_classes import *
+
 solids = [ [1, 1, 1, 1, 1, 1, 1],
            [1, 0, 4, 1, 0, 0, 1],
            [1, 0, 4, 1, 4, 0, 1],
@@ -48,13 +50,25 @@ def change_board(movement) :
         player.mine(movement)
         entity.destroy()
 
-    elif isinstance(entity, Barrel) or isinstance(entity, Minecart) :
+    elif isinstance(entity, Barrel) :
         target_pos = position_after_move(given_pos, movement)
 
         # check if the entity can be pushed
-        if isinstance(entity, Minecart) and solid_on(target_pos) < 3 :
+        if solid_on(target_pos) == 1 :
             return
-        elif isinstance(entity, Barrel) and solid_on(target_pos) == 1 :
+        if entity_on(target_pos) != 0 and not isinstance(entity_on(target_pos), Spider) :
+            return
+        
+        change_entity(target_pos, entity)
+        change_entity(given_pos, 0)
+        player.push(movement)
+        entity.move(movement)
+
+    elif isinstance(entity, Minecart) :
+        target_pos = position_after_move(given_pos, movement)
+
+        # check if the entity can be pushed
+        if solid_on(target_pos) < 3 :
             return
         if entity_on(target_pos) != 0 and not isinstance(entity_on(target_pos), Spider) :
             return
@@ -151,140 +165,6 @@ def binary_board() :
 def find_best_movement(pos) :
     return
 
-
-class Player :
-    def __init__(self, pos, orientation = (1, 0)) :
-        self.pos = pos
-        self.orientation = orientation
-        self.alive = True
-    
-    def move(self, movement) :
-        self.rotate(movement)
-        self.walk(movement)
-
-    def mine(self, movement) :
-        self.rotate(movement)
-        #animation minage
-    
-    def push(self, movement) :
-        self.rotate(movement)
-        #animation lever les bras
-        self.walk(movement)
-        #animation baisser les bras
-
-    def die(self) :
-        self.alive = False
-        #animate dying
-
-    def walk(self, movement) :
-        self.pos = (self.pos[0] + movement[0], self.pos[1] + movement[1])
-        #animation avec translation de movement
-    
-    def rotate(self, movement) :
-        if movement[0] == self.orientation[0] or movement[1] == self.orientation[1] :
-            if movement[0] == self.orientation[0] and movement[1] == self.orientation[1] :
-                #animation sans rotation
-                return
-            else :
-                #animation avec rotation de 180° dans le sens trigo
-                return
-        else :
-            if movement[1] == self.orientation[0] and movement[0] == -self.orientation[1] :
-                #animation avec rotation de 90° dans le sens trigo
-                return
-            else :
-                #animation avec rotation de -90° dans le sens trigo
-                return
-        self.orientation = movement
-
-
-class Spider :
-    def __init__(self, pos, orientation = (1, 0)) :
-        self.pos = pos
-        self.orientation = orientation
-    
-    def move(self, movement) :
-        self.rotate(movement)
-        self.walk(movement)
-
-    def attack(self) :
-        #animate attacking
-        return
-
-    def walk(self, movement) :
-        self.pos = (self.pos[0] + movement[0], self.pos[1] + movement[1])
-        #animation avec translation de movement
-    
-    def rotate(self, movement) :
-        if movement[0] == orientation[0] or movement[1] == orientation[1] :
-            if movement[0] == orientation[0] and movement[1] == orientation[1] :
-                #animation sans rotation
-                return
-            else :
-                #animation avec rotation de 180° dans le sens trigo
-                return
-        else :
-            if movement[1] == orientation[0] and movement[0] == -orientation[1] :
-                #animation avec rotation de 90° dans le sens trigo
-                return
-            else :
-                #animation avec rotation de -90° dans le sens trigo
-                return
-        self.orientation = movement
-
-
-class Ore :
-    def __init__(self, pos) :
-        self.pos = pos
-    
-    def destroy(self) :
-        #animation de destruction
-        self.__del__()
-
-    def __del__(self):
-        return
-
-
-class Barrel :
-    def __init__(self, pos) :
-        self.pos = pos
-    
-    def move(self, movement, src_rail, dst_rail) :
-        self.pos = (self.pos[0] + movement[0], self.pos[1] + movement[1])
-        #animation avec translation de movement
-
-
-class Minecart :
-    def __init__(self, pos, rail = 3) :
-        self.pos = pos
-        # TODO : modifier l'initialisation de l'orientation
-        self.rail = 3
-    
-    def move(self, movement, src_rail, dst_rail) :
-        self.pos = (self.pos[0] + movement[0], self.pos[1] + movement[1])
-        for rail in (src_rail, dst_rail) :
-            if rail in (3, 4) :
-                self.linear_roll(movement)
-            else :
-                self.rotative_roll(movement, rail)
-                
-    def linear_roll(self, movement) :
-        #animation avec translation de movement/2
-        return
-    
-    def rotative_roll(self, movement, rail) :
-        if movement[0] == -1 :
-            trigo_rotation = rail in (5, 6)
-        if movement[0] == 1 :
-            trigo_rotation = rail in (7, 8)
-        if movement[1] == -1 :
-            trigo_rotation = rail in (6, 8)
-        if movement[1] == 1 :
-            trigo_rotation = rail in (5, 7)
-        
-        rotation_center = ((rail-5)%2-1/2, (rail-5)//2-1/2)
-        #animation avec rotation d'angle 45° dans le sens trigo si trigo_rotation est à True
-        #autour de rotation_center
 
 def print_board() :
     print("")
